6a330bf8232a351223328124003e0c39202f79a9
Anthony Lee <anthonylee1994@users.noreply.github.com>
2024-09-04 10:51:23 +0800

    feat: add avg price ratio

--- src/types/StockForecast.ts
+++ src/types/StockForecast.ts
@@ -8,26 +8,39 @@ export interface StockForecast {
     sps_ttm: string;
     nav_ttm: string;
     ocf_ttm: string;
-    pb_forecast_discount: string;
-    pb_high: string;
-    pb_high_forecast: string;
-    pb_low: string;
-    pb_low_forecast: string;
-    pe_forecast_discount: string;
+
     pe_high: string;
-    pe_high_forecast: string;
+    pe_avg: string;
     pe_low: string;
+    pe_high_forecast: string;
+    pe_avg_forecast: string;
     pe_low_forecast: string;
-    pocf_forecast_discount: string;
-    pocf_high: string;
-    pocf_high_forecast: string;
-    pocf_low: string;
-    pocf_low_forecast: string;
-    ps_forecast_discount: string;
+    pe_forecast_discount: string;
+
     ps_high: string;
-    ps_high_forecast: string;
+    ps_avg: string;
     ps_low: string;
+    ps_high_forecast: string;
+    ps_avg_forecast: string;
     ps_low_forecast: string;
+    ps_forecast_discount: string;
+
+    pb_high: string;
+    pb_avg: string;
+    pb_low: string;
+    pb_high_forecast: string;
+    pb_avg_forecast: string;
+    pb_low_forecast: string;
+    pb_forecast_discount: string;
+
+    pocf_high: string;
+    pocf_avg: string;
+    pocf_low: string;
+    pocf_high_forecast: string;
+    pocf_avg_forecast: string;
+    pocf_low_forecast: string;
+    pocf_forecast_discount: string;
+
     updated_at: string;
     created_at: string;
 }
