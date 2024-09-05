25f13e64894add4f24cdc9bd6cb0f28ed89fe99a
Anthony Lee <anthonylee1994@users.noreply.github.com>
2024-09-05 15:59:30 +0800

    refactor: Update StockSummaryModal table column styles

--- src/app.tsx
+++ src/app.tsx
@@ -6,6 +6,7 @@ import {StockSummaryModal} from "./components/StockSummaryModal";
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-quartz.css";
 import {findGrowthRateByPE} from "./utils/growthRateToPE";
+import {ColDef} from "ag-grid-community";
 
 export const App = React.memo(() => {
     const [forecasts, setForecasts] = React.useState<StockForecast[]>([]);
@@ -64,26 +65,12 @@ export const App = React.memo(() => {
         fetchData();
     }, []);
 
-    const discountCellStyle = params => {
-        const value = Number(params.value);
-        if (value < 0) {
-            return {backgroundColor: "#ccc"};
-        } else if (value <= 1 && value > 0) {
-            return {backgroundColor: "#a6e194"};
-        } else if (value > 1 && value <= 1.5) {
-            return {backgroundColor: "#f3c08b"};
-        } else if (value > 1.5) {
-            return {backgroundColor: "#f08e8d"};
-        }
-        return null;
-    };
-
-    const ratioCellStyle = (valueFunction: (data: StockForecast) => number) => params => {
+    const ratioCellStyle = (valueFunction?: (value: number, data: StockForecast) => number) => params => {
         if (!params.data) {
             return null;
         }
 
-        const value = valueFunction(params.data);
+        const value = valueFunction ? valueFunction(params.value, params.data) : params.value;
         if (value < 0) {
             return {backgroundColor: "#ccc"};
         } else if (value <= 1 && value > 0) {
@@ -97,6 +84,113 @@ export const App = React.memo(() => {
 
     const growthRateTooltipValueGetter = params => `代表增長率: ${findGrowthRateByPE(Number(params.value))}%`;
 
+    const columnDefs = React.useMemo<ColDef<StockForecast>[]>(
+        () => [
+            {field: "symbol", pinned: "left", headerName: "代碼", filter: true, width: 100},
+            {field: "price", headerName: "股價", type: "rightAligned", width: 100},
+            {field: "volume", headerName: "成交量", type: "rightAligned", width: 150},
+            {field: "market_cap", headerName: "市值", type: "rightAligned", width: 150},
+            {field: "roe_ttm", headerName: "ROE", type: "rightAligned", width: 100},
+
+            {field: "pe_forecast_discount", headerName: "P/E 股價折現", type: "rightAligned", cellStyle: ratioCellStyle(), width: 120},
+            {field: "ps_forecast_discount", headerName: "P/S 股價折現", type: "rightAligned", cellStyle: ratioCellStyle(), width: 120},
+            {field: "pb_forecast_discount", headerName: "P/B 股價折現", type: "rightAligned", cellStyle: ratioCellStyle(), width: 120},
+            {field: "pocf_forecast_discount", headerName: "P/OCF 股價折現", type: "rightAligned", cellStyle: ratioCellStyle(), width: 140},
+
+            {
+                headerName: "P/E 值",
+                type: "centerAligned",
+                children: [
+                    {field: "eps_ttm", headerName: "每股盈利", type: "rightAligned", width: 100},
+                    {
+                        field: "pe_current",
+                        headerName: "現值",
+                        type: "rightAligned",
+                        width: 100,
+                        cellStyle: ratioCellStyle((_, data) => Number(data.pe_current) / Number(data.pe_avg)),
+                        tooltipValueGetter: growthRateTooltipValueGetter,
+                    },
+                    {field: "pe_high", headerName: "極值", type: "rightAligned", width: 100, tooltipValueGetter: growthRateTooltipValueGetter},
+                    {field: "pe_avg", headerName: "均值", type: "rightAligned", width: 100, tooltipValueGetter: growthRateTooltipValueGetter},
+                    {field: "pe_low", headerName: "殘值", type: "rightAligned", width: 100, tooltipValueGetter: growthRateTooltipValueGetter},
+                ],
+            },
+            {
+                headerName: "P/E 股價預測",
+                children: [
+                    {field: "price", headerName: "現值", type: "rightAligned", width: 100, cellStyle: ratioCellStyle((_, data) => Number(data.price) / Number(data.pe_avg_forecast))},
+                    {field: "pe_high_forecast", headerName: "極值", type: "rightAligned", width: 120},
+                    {field: "pe_avg_forecast", headerName: "均值", type: "rightAligned", width: 120},
+                    {field: "pe_low_forecast", headerName: "殘值", type: "rightAligned", width: 120},
+                ],
+            },
+
+            {
+                headerName: "P/S 值",
+                type: "centerAligned",
+                children: [
+                    {field: "sps_ttm", headerName: "每股營收", type: "rightAligned", width: 100},
+                    {field: "ps_current", headerName: "現值", type: "rightAligned", width: 100, cellStyle: ratioCellStyle((_, data) => Number(data.ps_current) / Number(data.ps_avg))},
+                    {field: "ps_high", headerName: "極值", type: "rightAligned", width: 100},
+                    {field: "ps_avg", headerName: "均值", type: "rightAligned", width: 100},
+                    {field: "ps_low", headerName: "殘值", type: "rightAligned", width: 100},
+                ],
+            },
+            {
+                headerName: "P/S 股價預測",
+                children: [
+                    {field: "price", headerName: "現值", type: "rightAligned", width: 100, cellStyle: ratioCellStyle((_, data) => Number(data.price) / Number(data.ps_avg_forecast))},
+                    {field: "ps_high_forecast", headerName: "極值", type: "rightAligned", width: 120},
+                    {field: "ps_avg_forecast", headerName: "均值", type: "rightAligned", width: 120},
+                    {field: "ps_low_forecast", headerName: "殘值", type: "rightAligned", width: 120},
+                ],
+            },
+
+            {
+                headerName: "P/B 值",
+                type: "centerAligned",
+                children: [
+                    {field: "nav_ttm", headerName: "每股淨值", type: "rightAligned", width: 100},
+                    {field: "pb_current", headerName: "現值", type: "rightAligned", width: 100, cellStyle: ratioCellStyle((_, data) => Number(data.pb_current) / Number(data.pb_avg))},
+                    {field: "pb_high", headerName: "極值", type: "rightAligned", width: 100},
+                    {field: "pb_avg", headerName: "均值", type: "rightAligned", width: 100},
+                    {field: "pb_low", headerName: "殘值", type: "rightAligned", width: 100},
+                ],
+            },
+            {
+                headerName: "P/B 股價預測",
+                children: [
+                    {field: "price", headerName: "現值", type: "rightAligned", width: 100, cellStyle: ratioCellStyle((_, data) => Number(data.price) / Number(data.pb_avg_forecast))},
+                    {field: "pb_high_forecast", headerName: "極值", type: "rightAligned", width: 120},
+                    {field: "pb_avg_forecast", headerName: "均值", type: "rightAligned", width: 120},
+                    {field: "pb_low_forecast", headerName: "殘值", type: "rightAligned", width: 120},
+                ],
+            },
+
+            {
+                headerName: "P/OCF 值",
+                type: "centerAligned",
+                children: [
+                    {field: "ocf_ttm", headerName: "每股營運現金流", type: "rightAligned", width: 130},
+                    {field: "pocf_current", headerName: "現值", type: "rightAligned", width: 130, cellStyle: ratioCellStyle((_, data) => Number(data.pocf_current) / Number(data.pocf_avg))},
+                    {field: "pocf_high", headerName: "極值", type: "rightAligned", width: 130},
+                    {field: "pocf_avg", headerName: "均值", type: "rightAligned", width: 130},
+                    {field: "pocf_low", headerName: "殘值", type: "rightAligned", width: 130},
+                ],
+            },
+            {
+                headerName: "P/OCF 股價預測",
+                children: [
+                    {field: "price", headerName: "現值", type: "rightAligned", width: 100, cellStyle: ratioCellStyle((_, data) => Number(data.price) / Number(data.pocf_avg_forecast))},
+                    {field: "pocf_high_forecast", headerName: "極值", type: "rightAligned", width: 140},
+                    {field: "pocf_avg_forecast", headerName: "均值", type: "rightAligned", width: 140},
+                    {field: "pocf_low_forecast", headerName: "殘值", type: "rightAligned", width: 140},
+                ],
+            },
+        ],
+        []
+    );
+
     return (
         <React.Fragment>
             <div className="ag-theme-quartz" style={{height: "calc(100vh - 20px)"}}>
@@ -108,109 +202,7 @@ export const App = React.memo(() => {
                             setSelectedItem(event.data);
                         }
                     }}
-                    columnDefs={[
-                        {field: "symbol", pinned: "left", headerName: "代碼", filter: true, width: 100},
-                        {field: "price", headerName: "股價", type: "rightAligned", width: 100},
-                        {field: "volume", headerName: "成交量", type: "rightAligned", width: 150},
-                        {field: "market_cap", headerName: "市值", type: "rightAligned", width: 150},
-                        {field: "roe_ttm", headerName: "ROE", type: "rightAligned", width: 100},
-
-                        {field: "pe_forecast_discount", headerName: "P/E 股價折現", type: "rightAligned", cellStyle: discountCellStyle, width: 120},
-                        {field: "ps_forecast_discount", headerName: "P/S 股價折現", type: "rightAligned", cellStyle: discountCellStyle, width: 120},
-                        {field: "pb_forecast_discount", headerName: "P/B 股價折現", type: "rightAligned", cellStyle: discountCellStyle, width: 120},
-                        {field: "pocf_forecast_discount", headerName: "P/OCF 股價折現", type: "rightAligned", cellStyle: discountCellStyle, width: 140},
-
-                        {
-                            headerName: "P/E 值",
-                            type: "centerAligned",
-                            children: [
-                                {field: "eps_ttm", headerName: "每股盈利", type: "rightAligned", width: 100},
-                                {
-                                    field: "pe_current",
-                                    headerName: "現值",
-                                    type: "rightAligned",
-                                    width: 100,
-                                    cellStyle: ratioCellStyle(data => Number(data.pe_current) / Number(data.pe_avg)),
-                                    tooltipValueGetter: growthRateTooltipValueGetter,
-                                },
-                                {field: "pe_high", headerName: "極值", type: "rightAligned", width: 100, tooltipValueGetter: growthRateTooltipValueGetter},
-                                {field: "pe_avg", headerName: "均值", type: "rightAligned", width: 100, tooltipValueGetter: growthRateTooltipValueGetter},
-                                {field: "pe_low", headerName: "殘值", type: "rightAligned", width: 100, tooltipValueGetter: growthRateTooltipValueGetter},
-                            ],
-                        },
-                        {
-                            headerName: "P/E 股價預測",
-                            children: [
-                                {field: "price", headerName: "現值", type: "rightAligned", width: 100, cellStyle: ratioCellStyle(data => Number(data.price) / Number(data.pe_avg_forecast))},
-                                {field: "pe_high_forecast", headerName: "極值", type: "rightAligned", width: 120},
-                                {field: "pe_avg_forecast", headerName: "均值", type: "rightAligned", width: 120},
-                                {field: "pe_low_forecast", headerName: "殘值", type: "rightAligned", width: 120},
-                            ],
-                        },
-
-                        {
-                            headerName: "P/S 值",
-                            type: "centerAligned",
-                            children: [
-                                {field: "sps_ttm", headerName: "每股營收", type: "rightAligned", width: 100},
-                                {field: "ps_current", headerName: "現值", type: "rightAligned", width: 100, cellStyle: ratioCellStyle(data => Number(data.ps_current) / Number(data.ps_avg))},
-                                {field: "ps_high", headerName: "極值", type: "rightAligned", width: 100},
-                                {field: "ps_avg", headerName: "均值", type: "rightAligned", width: 100},
-                                {field: "ps_low", headerName: "殘值", type: "rightAligned", width: 100},
-                            ],
-                        },
-                        {
-                            headerName: "P/S 股價預測",
-                            children: [
-                                {field: "price", headerName: "現值", type: "rightAligned", width: 100, cellStyle: ratioCellStyle(data => Number(data.price) / Number(data.ps_avg_forecast))},
-                                {field: "ps_high_forecast", headerName: "極值", type: "rightAligned", width: 120},
-                                {field: "ps_avg_forecast", headerName: "均值", type: "rightAligned", width: 120},
-                                {field: "ps_low_forecast", headerName: "殘值", type: "rightAligned", width: 120},
-                            ],
-                        },
-
-                        {
-                            headerName: "P/B 值",
-                            type: "centerAligned",
-                            children: [
-                                {field: "nav_ttm", headerName: "每股淨值", type: "rightAligned", width: 100},
-                                {field: "pb_current", headerName: "現值", type: "rightAligned", width: 100, cellStyle: ratioCellStyle(data => Number(data.pb_current) / Number(data.pb_avg))},
-                                {field: "pb_high", headerName: "極值", type: "rightAligned", width: 100},
-                                {field: "pb_avg", headerName: "均值", type: "rightAligned", width: 100},
-                                {field: "pb_low", headerName: "殘值", type: "rightAligned", width: 100},
-                            ],
-                        },
-                        {
-                            headerName: "P/B 股價預測",
-                            children: [
-                                {field: "price", headerName: "現值", type: "rightAligned", width: 100, cellStyle: ratioCellStyle(data => Number(data.price) / Number(data.pb_avg_forecast))},
-                                {field: "pb_high_forecast", headerName: "極值", type: "rightAligned", width: 120},
-                                {field: "pb_avg_forecast", headerName: "均值", type: "rightAligned", width: 120},
-                                {field: "pb_low_forecast", headerName: "殘值", type: "rightAligned", width: 120},
-                            ],
-                        },
-
-                        {
-                            headerName: "P/OCF 值",
-                            type: "centerAligned",
-                            children: [
-                                {field: "ocf_ttm", headerName: "每股營運現金流", type: "rightAligned", width: 130},
-                                {field: "pocf_current", headerName: "現值", type: "rightAligned", width: 130, cellStyle: ratioCellStyle(data => Number(data.pocf_current) / Number(data.pocf_avg))},
-                                {field: "pocf_high", headerName: "極值", type: "rightAligned", width: 130},
-                                {field: "pocf_avg", headerName: "均值", type: "rightAligned", width: 130},
-                                {field: "pocf_low", headerName: "殘值", type: "rightAligned", width: 130},
-                            ],
-                        },
-                        {
-                            headerName: "P/OCF 股價預測",
-                            children: [
-                                {field: "price", headerName: "現值", type: "rightAligned", width: 100, cellStyle: ratioCellStyle(data => Number(data.price) / Number(data.pocf_avg_forecast))},
-                                {field: "pocf_high_forecast", headerName: "極值", type: "rightAligned", width: 140},
-                                {field: "pocf_avg_forecast", headerName: "均值", type: "rightAligned", width: 140},
-                                {field: "pocf_low_forecast", headerName: "殘值", type: "rightAligned", width: 140},
-                            ],
-                        },
-                    ]}
+                    columnDefs={columnDefs}
                 />
             </div>
             <StockSummaryModal item={selectedItem} onClose={() => setSelectedItem(null)} />
